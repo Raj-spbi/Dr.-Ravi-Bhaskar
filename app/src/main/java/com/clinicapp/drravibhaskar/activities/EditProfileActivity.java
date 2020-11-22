f66ccc7b02559dcbe54febb6e89198995ec21392
rajesh7055 <68139839+rajesh7055@users.noreply.github.com>
2020-11-22 16:00:57 +0530

    some code

---
+++ app/src/main/java/com/clinicapp/drravibhaskar/activities/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.clinicapp.drravibhaskar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.clinicapp.drravibhaskar.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
